882564febe3db3b7d29d610a04551e9cbc95e5e6
fms <fms@bizet>
2015-08-14 02:54:57 -0500

    scaffolding HTML

--- resources/views/app.php
+++ resources/views/app.php
@@ -1,11 +1,14 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>KSD inventory example</title>
-    </head>
-    <body>
-        <h1>KSD Inventory Example</h1>
-            <table>
-            <tr><th>Name</th><th>Serial</th><th>Type</th><th>Room</th><th>City</th><th>Date</th></tr>
+<head>
+    <title>KSD inventory example</title>
+</head>
+<body>
+<h1>KSD Inventory Example</h1>
+<?php //search ?>
+
+<table>
+<tr><th>Name</th><th>Serial</th><th>Type</th><th>Room</th><th>City</th><th>Date</th></tr>
 <?
 // generate table
 
@@ -16,7 +19,6 @@
 ?>
 
 
-        <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    </body>
-</html>
+<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body></html>
